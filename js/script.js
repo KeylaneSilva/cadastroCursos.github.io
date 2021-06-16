cc4d90e7d1b0e28e9f2912fa462059ecdf0e28c3
Keylane <keylanessilva07@gmail.com>
2021-06-16 02:24:14 -0300

    adicionando novas features

--- js/script.js
+++ js/script.js
@@ -1,26 +1,18 @@
 document.getElementById('formulario').addEventListener('submit', cadastrarCurso)
 
-function cadastrarCurso(e){
-    nomeCurso = document.getElementById('form_nome').value
-    dataIncioCurso = document.getElementById('form_dataInicio').value
-    dataFimCurso = document.getElementById('form_dataFim').value
-    duracaoCurso = document.getElementById('form_duracao').value
-    descricaoCurso = document.getElementById('form_desc').value
-
-    //verificando campo vazios
-    if(!nomeCurso || !dataIncioCurso || !dataFimCurso || !duracaoCurso || !descricaoCurso){
-        alert("Preencha os campos")
-        
-    }
+function cadastrarCurso(e) {
+    nomeCurso = document.getElementById('form-nome').value
+    dataIncioCurso = document.getElementById('form-dataInicio').value
+    dataFimCurso = document.getElementById('form-dataFim').value
+    duracaoCurso = document.getElementById('form-duracao').value
+    descricaoCurso = document.getElementById('form-desc').value
 
     //verificação da data
-    if(dataFimCurso < dataIncioCurso){
-        alert("Data não permitida")
-        
+    if (dataFimCurso < dataIncioCurso) {
+        Alert.render("Data não permitida")
+        e.preventDefault()
+        return 
     }
-    
-    //limpando campos
-    /*document.getElementById('formulario').reset()*/
 
     var curso = {
         nome: nomeCurso,
@@ -31,37 +23,85 @@ function cadastrarCurso(e){
     }
 
     //salva os dados no navegador
-    if(localStorage.getItem('listaCursos') === null){
+    if (localStorage.getItem('listaCursos') === null) {
         var cursos = []
         cursos.push(curso)
         localStorage.setItem('listaCursos', JSON.stringify(cursos))
-    }else{
+    } else {
         var cursos = JSON.parse(localStorage.getItem('listaCursos'))
         cursos.push(curso)
         localStorage.setItem('listaCursos', JSON.stringify(cursos))
     }
 
+    Alert.render('Curso cadastrado com sucesso')
     e.preventDefault()
     mostrarCadastros()
+
+    //limpando campos
+    document.getElementById('formulario').reset()
 }
 
 
-function mostrarCadastros(){
+function mostrarCadastros() {
     var cursos = JSON.parse(localStorage.getItem('listaCursos'))
     var listaResultados = document.getElementById('resultado')
 
     listaResultados.innerHTML = '';
 
-    for(var i = 0; i < cursos.length; i++){
+    for (var i = 0; i < cursos.length; i++) {
         var nome = cursos[i].nome;
         var inicio = cursos[i].dataInicio;
         var fim = cursos[i].dataFinal;
         var duracao = cursos[i].duracao;
         var descricao = cursos[i].descricao;
 
-        listaResultados.innerHTML += '<tr><td>' + nome +
-                                    '</td><td>' + duracao + '</td><td>' + inicio +
-                                    '</td><td>' + fim +
-                                    '</td></tr>'
+        listaResultados.innerHTML += '<tr class="tbodyTr" ondblclick="apagar(\'' + nome + '\')"><td class="nomeCurso" >' + nome +
+                                        '</td><td>' + duracao + 
+                                        '</td><td>' + inicio +
+                                        '</td><td>' + fim +
+                                        '</td></tr>'
     }
 }
+
+/* filtro */
+
+var campoFiltro = document.querySelector('#pesquisa-cursos')
+
+campoFiltro.addEventListener('input', function(){
+    var cursos = document.querySelectorAll(".tbodyTr")
+
+    if(this.value.length > 0){
+        
+        for(var i = 0; i < cursos.length; i++){
+            var curso = cursos[i];
+            var nomeCurso = curso.querySelector(".nomeCurso")
+            var nome = nomeCurso.textContent
+            var expressao = new RegExp(this.value, "i")
+
+            if(!expressao.test(nome)){
+                curso.classList.add("invisivel")
+            }else{
+                curso.classList.remove("invisivel")
+            }
+        }
+    }else{
+        for(var i = 0; i < cursos.length; i++){
+            var curso = cursos[i]
+            curso.classList.remove("invisivel")
+        }
+    }
+})
+
+/* apagar */
+    function apagar(nome){
+        var cursos = JSON.parse(localStorage.getItem('listaCursos'))
+        
+        for(var i = 0; i < cursos.length; i++){
+            if(cursos[i].nome == nome){
+                cursos.splice(i, 1);
+            }
+            localStorage.setItem('listaCursos', JSON.stringify(cursos))
+            mostrarCadastros()
+        }
+    }
+        
